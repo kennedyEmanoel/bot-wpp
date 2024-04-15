572085c12ed8cead04aadae54c9160150636e070
Kennedy Emanoel <kennedy1emanoel19@gmail.com>
2024-04-15 12:55:54 -0300

    closing contact

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import os
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import requests
+
+from openai import OpenAI
+client = OpenAI()
 
 dir_path = os.getcwd()
 chrome_options = Options()
@@ -19,6 +21,8 @@ time.sleep(15)
 
 def bot():
     try:
+        # PROCURANDO NOTIFICAÇÃO
+        time.sleep(5)
         noti = driver.find_elements(By.CLASS_NAME, 'x173ssrc')
         last_noti = noti[-1]
         noti_action = webdriver.common.action_chains.ActionChains(driver)
@@ -27,7 +31,25 @@ def bot():
         noti_action.perform()
         noti_action.click()
         noti_action.perform()
+
+        # CLASS_NAME _akbu
+        all_msg = driver.find_elements(By.CLASS_NAME, '_akbu')
+        all_text_msg = [e.text for e in all_msg]
+        msg = all_text_msg[-1]
+        print(msg)
         time.sleep(5)
+
+        # XPATH //*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p
+        text_field = driver.find_element(
+            By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
+        text_field.click()
+        time.sleep(3)
+        text_field.send_keys('bot-wpp', Keys.ENTER)
+        time.sleep(2)
+
+        # FECHAR CONTATO
+        webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+
     except:
         print('buscando novas notificações')
 
